9418fee3fb34aa310679f184c72ae3c5c20990f7
Nir132 <86047742+Nir132@users.noreply.github.com>
2023-07-13 09:45:51 +0300

    add video and scroll property to main page

---
+++ main.js
@@ -0,0 +1,19 @@
+const navElement = document.querySelector("nav");
+const textElement = document.querySelector(".text_element");
+const navliElements = document.querySelectorAll("nav li");
+
+window.addEventListener("scroll",function(){
+    if (window.scrollY > 200) {
+        navElement.classList.add("scrolled");
+        textElement.style.color = "#D3C3DC";
+        navliElements.forEach(function(navliElement) {
+            navliElement.style.color = "#D3C3DC";
+        });
+      } else {
+        navElement.classList.remove("scrolled");
+        textElement.style.color = "#291A2E";
+        navliElements.forEach(function(navliElement) {
+          navliElement.style.color = "#291A2E";
+      });
+      }
+    });
