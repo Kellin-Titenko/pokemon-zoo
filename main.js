2cfff8911e69c3b07d9c1ac258ee86c0bd35f6f5
Nir132 <86047742+Nir132@users.noreply.github.com>
2023-07-13 17:08:41 +0300

    accidently made changes on main

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const navElement = document.querySelector("nav");
-const textElement = document.querySelector(".text_element");
-const navliElements = document.querySelectorAll("nav li");
+const textElement = document.querySelector(".text_element a");
+const navliAElements = document.querySelectorAll("nav li a");
 var videos = document.getElementsByClassName('video');
 var currentVideoIndex = 0;
 
 window.addEventListener("scroll",function(){
     if (window.scrollY > 200) {
         navElement.classList.add("scrolled");
-        textElement.style.color = "#D3C3DC";
-        navliElements.forEach(function(navliElement) {
-            navliElement.style.color = "#D3C3DC";
+        // textElement.style.color = "#D3C3DC";
+        textElement.style.color = "#932f6d";
+        navliAElements.forEach(function(navliElement) {
+            navliElement.style.color = "#932f6d";
         });
       } else {
         navElement.classList.remove("scrolled");
         textElement.style.color = "#291A2E";
-        navliElements.forEach(function(navliElement) {
+        navliAElements.forEach(function(navliElement) {
           navliElement.style.color = "#291A2E";
       });
       }
