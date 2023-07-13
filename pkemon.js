ff81358799cb0d327c14b7c108cd01054070092f
kellin titenko <xkellinx@gmail.com>
2023-07-13 13:02:03 +0300

    day one

---
+++ pkemon.js
@@ -0,0 +1,13 @@
+const button_info = document.getElementById("info_butten");
+button_info.addEventListener("click", fun_clik_pokemon1);
+function fun_clik_pokemon1() {
+    console.log("hi!")
+  window.location.href = "kellin1.html";
+ }
+
+ const button = document.getElementById("pokemons_butten");
+button.addEventListener("click", fun_clik_pokemon2);
+function fun_clik_pokemon2() {
+    console.log("hi!")
+  window.location.href = "pokemon_kellin.html";
+ }
